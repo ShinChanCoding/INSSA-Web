2a3d057f9d9ef65aa6c9451ff9008958bde25532
gyongyeon <126961013+Cloudyee@users.noreply.github.com>
2023-05-03 12:08:13 +0900

    Update Inssa.js
    버튼 누르면 progress bar동작+일정시간 버튼 기능 무력화 기능 추가

--- Inssa.js
+++ Inssa.js
@@ -9,6 +9,12 @@ let imageAfter = document.querySelector(".image-after");
 let imageBeforeSM = document.querySelector(".image-before");
 let saveBtn = document.getElementById("saveBtn");
 
+const myBar = document.querySelector('.my-Bar');
+const progress = document.getElementById('progress');
+const buttons = document.querySelectorAll('button');
+let progressValue = 0;
+let intervalId; // 타이머 ID를 저장할 변수를 생성합니다.
+
 const reader = new FileReader();
 const formData = new FormData();
 let file = null;
@@ -58,3 +64,29 @@ startBtn.addEventListener("click", () => {
 saveBtn.addEventListener("click", () => {
     readAsDataURL(blob);
 });
+
+progress.addEventListener('click', () => {
+    buttons.forEach((button) => {
+        button.disabled = true;
+        button.style.pointerEvents = "none"; // 버튼의 마우스 이벤트 처리를 무력화합니다.
+    });
+    setTimeout(() => {
+        buttons.forEach((button) => {
+            button.disabled = false;
+            button.style.pointerEvents = "auto"; // 버튼의 마우스 이벤트 처리를 다시 활성화합니다.
+        });
+    }, 9000);
+    myBar.setAttribute('id', 'play');
+    runProgressBar();
+});
+
+function runProgressBar() {
+    intervalId = setInterval(() => {
+        if (progressValue < 100) {
+            progressValue++;
+            myBar.style.width = `${progressValue}%`;
+        } else {
+            clearInterval(intervalId);
+        }
+    }, 30);
+}
